a774ae867a61d45688d3b615ddb6149a654e0c75
Alex McHugh <alexmchughnz@gmail.com>
2020-02-19 16:25:45 +1300

    Convert patient data format.

--- 1. Detemir/My Code/extractdata.m
+++ 1. Detemir/My Code/extractdata.m
@@ -3,15 +3,39 @@ patientNums = [1 3 4];
 
 
 % STUB: grab data from previous code structs
-for ii = ii = 1:length(patientNums)
-load sys
-data = sys.Data
-
-CPep
-
-
-
-
+for ii = 1:length(patientNums)
+    patientNum = patientNums(ii);
+    filename = sprintf("sys%d.mat", patientNum);
+    load(filename);    
+    data = sys.Data;
+    
+    
+    P.patientNum = data.PtNo;
+    P.mass = data.pt_mass;
+    
+    P.CPep.value = data.Cpep;
+    P.CPep.time = data.Cpep_time;    
+    
+    P.G{1}.value = data.bg1;
+    P.G{1}.time = data.bg1_time;
+    P.G{2}.value = data.bg2;
+    P.G{2}.time = data.bg2_time;
+    P.G{3}.value = data.bg3;
+    P.G{3}.time = data.bg3_time;
+    
+    P.I.value = data.PlasmaI;
+    P.I.time  = data.PlasmaI_time;
+    
+    P.meal.duration = data.meal_durations;
+    P.meal.startTime = data.meal_start;
+    P.meal.carbs = data.carbs;
+    P.meal.sugar = data.sugar;    
+    
+    
+    filename = sprintf("patient%d.mat", patientNum);
+    save(filename, '-struct', 'P');
+    clear P
+end
 %\STUB
 
 

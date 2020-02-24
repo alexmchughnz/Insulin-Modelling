ce65e255d4e93890d5d1cf3e0e99d5b761caae72
Alex McHugh <alexmchughnz@gmail.com>
2020-02-24 15:07:52 +1300

    GetGlucoseDelivery finished and verified.

--- 1. Detemir/My Code/Functions/GetGlucoseDelivery.m
+++ 1. Detemir/My Code/Functions/GetGlucoseDelivery.m
@@ -6,15 +6,15 @@ function D = GetGlucoseDelivery(t, P)
 % OUTPUT:
 %   D   - glucose delivery rate at time t
 
-% Extract meal times in minutes.
+% Extract meal times.
 M = P.meal;
 mealStarts = M.startTimes;              % [min]
 mealEnds = M.startTimes + M.durations;  % [min]
 
 % Add contribution from all meals occuring during this time.
-MAGIC_SCALE_FACTOR = 1000/180.156; % From eating.m.
-currentMeals = (mealStarts < t) & (t < mealEnds); % [logical]
-D = dot(currentMeals, M.carbs./M.durations*MAGIC_SCALE_FACTOR); % Glucose delivery rate [g/min?]
+MAGIC_SCALE_FACTOR = 1000/180.156;                               % From eating.m.
+currentMeals = (mealStarts < t) & (t < mealEnds);                % Which meals contribute at time=t? [logical]
+D = dot(currentMeals, M.carbs./M.durations*MAGIC_SCALE_FACTOR);  % Glucose delivery rate [g/min?]
 
 end
 

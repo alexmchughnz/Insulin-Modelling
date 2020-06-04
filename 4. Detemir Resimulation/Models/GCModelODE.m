f485d70f52ad941ee97161d0d68a28de35646c11
Alex McHugh <alexmchughnz@gmail.com>
2020-06-05 11:32:59 +1200

    Separated ODEs into their own function files.

---
+++ 4. Detemir Resimulation/Models/GCModelODE.m
@@ -0,0 +1,51 @@
+function [dY] = GCModelODE(t, Y, P, Y0)
+% ODE for GC model. Use with ode45.
+% Requires P1(t) and P2(t) - must be run AFTER GI model.
+% INPUTS:
+%   t   - time at which to evaluate ODE
+%   Y   - states [P1; P2; G; I; Q] at time == t
+%   P   - patient struct
+%   Y0  - initial conditions of states
+% OUTPUT:
+%   dY  - derivatives of states at time == t
+
+global GI GC
+
+%% Input
+G  = Y(1);
+I  = Y(2);
+Q  = Y(3);
+Q0 = Y0(3);
+
+%% Variables
+% Time dependent.
+n = (1 + floor(t));  % Index of current timestep.
+SI        = P.SI(n);
+Uen       = P.Uen.value(n);
+P2        = P.results.P2(n);
+GInfusion = P.GInfusion(n);   % Glucose infusion rate [mol/min]
+GFast     = P.GFast(t);       % Fasting glucose [mol?/L]
+
+% Patient dependent.
+VG  = GC.VG(P);
+VI  = GC.VI(P);
+VQ  = GC.VQ(P);
+ 
+% Derived values.
+QTFast  = Q0 + P.results.QDF(1);
+QT      = Q + P.results.QDF(n);
+
+%% Computation
+dG  = -GC.pg*(G-GFast) ...
+          - SI*(G*QT - GFast*QTFast)/(1 + GC.alphaG*QT) ...
+          + GI.d2/VG*P2 + GInfusion/VG;      
+dI  = -GC.nK*I - P.nL/(1 + GC.alphaI*I)*I - GC.nI/VI*(I-Q) ...
+          + Uen*(1 - P.xL)/VI;
+dQ  = GC.nI/VQ*(I-Q) - GC.nC*Q;
+
+%% Output
+dY = [dG;
+      dI;
+      dQ];
+
+end
--- 4. Detemir Resimulation/Models/GIModel.m
+++ 4. Detemir Resimulation/Models/GIModel.m
@@ -20,33 +20,3 @@ P.results.P1 = Y(:,1);
 P.results.P2 = Y(:,2);
 
 end
-
-
-function [dY] = GIModelODE(t, Y, P)
-% ODE for GI model. Use with ode45.
-% INPUTS:
-%   t   - time at which to evaluate ODE
-%   Y   - states [P1; P2] at time == t
-%   P   - patient struct
-% OUTPUT:
-%   dY  - derivatives of states at time == t
-
-global GI
-
-%% Input
-P1     = Y(1);
-P2     = Y(2);
-
-%% Variables
-% Patient dependent.
-D = GetGlucoseDelivery(t, P);
-
-%% Computation
-dP1 = -GI.d1*P1 + D;
-dP2 = GI.d1*P1 - GI.d2*P2;
-
-%% Output
-dY = [dP1;
-      dP2];
-
-end

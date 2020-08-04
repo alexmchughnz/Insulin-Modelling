e39ee24c66cee1ad868df1c3200d30a105f579ca
Alex McHugh <alexmchughnz@gmail.com>
2020-08-05 10:02:33 +1200

    Edited Uen for conformity with PDD/Jen

--- 4. Detemir Resimulation/Functions/EstimateInsulinSecretion.m
+++ 4. Detemir Resimulation/Functions/EstimateInsulinSecretion.m
@@ -16,7 +16,7 @@ global DEBUGPLOTS
 % Time of reading in sim [min]
 % Concentraton of C-peptide [pmol/L]
 [tCPep, vCPep] = GetSimTime(P, P.data.CPep);
-vCPep = vCPep * GC.VI; % Total C-peptides [pmol]
+% vCPep = vCPep * GC.VI; % Total C-peptides [pmol]
 
 % Rate constants.
 k1 = SC.k1;   
@@ -27,19 +27,28 @@ k3 = SC.k3;
 ppCPep = griddedInterpolant(tCPep, vCPep);
 
 % Make 1-minute spaced time vector, and interpolate CPep values.
-t = P.results.tArray;  % Time range [min]
-dt = t(2) - t(1);
-vCPep = ppCPep(t);
+tArray = [P.data.simTime(1) : P.data.simTime(end)]';  % Time range [min]
+dt = tArray(2) - tArray(1);
+vCPep = ppCPep(tArray);
 
 %% Solving
-% Calculate Y over time. Assumes dY/dt == 0;
-Y = zeros(size(t));
-Y(1) = k1(1)/k2*vCPep(1);
-Y = exp(-k2*t) .* (Y(1) + cumtrapz(t, exp(k2*t).*k1.*vCPep));
+% Initial conditions.
+Y = zeros(size(tArray));
+Y0 = k1/k2*vCPep(1);
+Y(1:2) = Y0;
+
+% Calculate Y over time. Assumes dY/dt == 0(?)
+for ii = 3:length(tArray)
+    tSeg = tArray(1:ii-1);
+    CPepSeg = vCPep(1:ii-1);
+    YSeg = Y(1:ii-1);
+    Y(ii) = Y(1) + trapz(tSeg, k1*CPepSeg - k2*YSeg);
+end
 
 % Calculate endogenous secretion rate (Uen).
-S = [diff(vCPep); 0]/dt + (k1 + k3).*vCPep - k2*Y;  % C-peptide secretion [pmol/min]
-Uen = C.pmol2mU(S);                                 % Endogenous insulin secretion [mU/min]
+dvCPep = [diff(vCPep)/dt; 0];
+S = (dvCPep + (k1+k3).*vCPep - k2*Y);  % C-peptide secretion [(pmol/L)/min]
+Uen = C.pmol2mU(S) * GC.VI;            % Endogenous insulin secretion [mU/min]
 
 % Write value to patient struct.
 P.results.Uen = Uen;
@@ -49,7 +58,7 @@ fprintf('P%d: Estimated Uen.\n', P.patientNum);
 DP = DEBUGPLOTS.EstimateInsulinSecretion;
 if DP.Uen
    MakeDebugPlot(P, DP);
-   plot(t, Uen)
+   plot(tArray, Uen)
    title(sprintf("%s: Uen", P.patientCode))
    
    xlabel("Time [min]")

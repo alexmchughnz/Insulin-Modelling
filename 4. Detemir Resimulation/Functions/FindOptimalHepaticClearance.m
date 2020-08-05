ac83d6e3cc0a5a039644eccce80e26bccadcdd1d
Alex McHugh <alexmchughnz@gmail.com>
2020-08-05 14:17:34 +1200

    BIG overhaul of FindOptimalHepaticClearance. Got rid of most useless functionality. Fixed some unit issues.

--- 4. Detemir Resimulation/Functions/FindOptimalHepaticClearance.m
+++ 4. Detemir Resimulation/Functions/FindOptimalHepaticClearance.m
@@ -4,19 +4,11 @@ function P = FindOptimalHepaticClearance(P, method, varargin)
 % INPUT:
 %   P        - patient struct
 %   method   - 'grid' to perform grid search
-%              'line' to perform grid search on line
-%              '2dline' to perform 2D search on line
 %              'load' to load previously-generated residuals data
-%              'improve' to load data and iterate it to some precision
 %   varargin - with 'grid', {1} nL bounds, and
 %                           {2} xL bounds to search over
 %                           {3} desired [nL, xL] grid precision
-%            - with 'line'/'2dline', {1} nL-intercept, and
-%                                    {2} xL-intercept to search between
-%                                    {3} desired [nL, xL] grid precision
-%            - with 'load', the filename to load
-%            - with 'improve', {1} the filename to load and improve
-%                              {2} desired [nL, xL] grid precision
+%            - with 'load', the filename to load (optional)
 % OUTPUT:
 %   P   - modified patient struct with nL and xL
 
@@ -24,18 +16,10 @@ global DEBUGPLOTS
 global FILEFORMAT
 
 GRIDFORMAT = "grid nL[%g %g]@%g xL[%g %g]@%g";
-LINEFORMAT = "line nL=%g@%g to xL=%g@%g, t=%g";
-LINE2DFORMAT = "2dline nL=%g@%g to xL=%g";
 FILEFORMAT = '%s_%s.mat';
-
-nLtoxLLineFun = @(nLIntercept, xLIntercept, nLDelta) ...
-    (@(nL) RoundToMultiple(xLIntercept - xLIntercept/nLIntercept * nL, ...
-    nLDelta));
-
 %% Setup
 if isequal(method, 'grid')
-    type = 'grid';
-    
+    % Set up search bounds.
     nLBounds = varargin{1};
     xLBounds = varargin{2};
     delta = varargin{3};
@@ -48,224 +32,33 @@ if isequal(method, 'grid')
     
     [xLGrid, nLGrid] = meshgrid(xLRange, nLRange);
     
-    savename = sprintf(GRIDFORMAT, ...
+    
+    filename = sprintf(GRIDFORMAT, ...
         nLBounds, nLDelta, xLBounds, xLDelta);
-    IResiduals = EvaluateGrid(P, nLGrid, xLGrid, savename);
+    IResiduals = EvaluateGrid(P, nLGrid, xLGrid, filename);
     
-elseif isequal(method, 'line')
-    type = 'line';
-    
-    nLIntercept = varargin{1};
-    xLIntercept = varargin{2};
-    delta = varargin{3};
-    
-    % Establish nL range.
-    nLDelta = delta(1);
-    xLDelta = delta(end);
-    
-    nLBounds = [0 1];
-    nLRange = nLBounds(1) : nLDelta : nLBounds(end);
-    
-    % Define line - a series of slices of xL for each nL value.
-    nLtoxL = nLtoxLLineFun(nLIntercept, xLIntercept, nLDelta);
-    
-    thickness = 0.2;  % In xL axis [-]
-    xLLine = [];
-    nLLine = [];
-    for nL = nLRange
-        xL = nLtoxL(nL);  % Corresponding xL on line for nL.
-        xLSlice = xL-thickness/2 : xLDelta : xL+thickness/2;
-        nLSlice = nL * ones(size(xLSlice));
-        
-        % Append to values along line.
-        xLLine = [xLLine xLSlice];
-        nLLine = [nLLine nLSlice];
-    end
-    
-    % Find residuals along nL/xL ranges.
-    savename = sprintf(LINEFORMAT, ...
-        nLIntercept, nLDelta, xLIntercept, xLDelta, thickness);
-    LineResiduals = EvaluateGrid(P, nLLine, xLLine, savename);
-    
-    % Reshape residuals onto grid.
-    xLRange = sort(unique(xLLine));
-    
-    [xLGrid, nLGrid] = meshgrid(xLRange, nLRange);
-    IResiduals = nan(size(nLGrid));
-    for ii = 1 : length(LineResiduals)
-        iinL = find(nLRange == nLLine(ii));
-        iixL = find(xLRange == xLLine(ii));
-        
-        IResiduals(iixL, iinL) = LineResiduals(ii);
-    end
-    
-    
-elseif isequal(method, '2dline')
-    type = '2dline';
-    
-    nLIntercept = varargin{1};
-    xLIntercept = varargin{2};
-    delta = varargin{3};
-    
-    % Establish full grid over area.
-    nLDelta = delta(1);
-    nLBounds = [0 1];
-    nLGrid = nLBounds(1) : nLDelta : nLBounds(end);
-    
-    nLtoxL = nLtoxLLineFun(nLIntercept, xLIntercept, nLDelta);
-    xLGrid = nLtoxL(nLGrid);
-    
-    % Find residuals along nL/xL line.
-    savename = sprintf(LINE2DFORMAT, ...
-        nLIntercept, nLDelta, xLIntercept);
-    IResiduals = EvaluateGrid(P, nLGrid, xLGrid, savename);
     
 elseif isequal(method, 'load')
-    % Load by name.
-    loadname = varargin{1};
-    load(ResultsPath(sprintf(FILEFORMAT, P.patientCode, loadname)), ...
-        'nLGrid', 'xLGrid', 'IResiduals');
-    
-    words = split(loadname, ' ');
-    type = words{1};
-    if type == "line"
-        % Reshape residuals onto grid.
-        nLLine = nLGrid;
-        nLRange = sort(unique(nLLine));
-        xLLine = xLGrid;
-        xLRange = sort(unique(xLLine));
-        [xLGrid, nLGrid] = meshgrid(xLRange, nLRange);
-        
-        LineResiduals = IResiduals;
-        
-        IResiduals = nan(numel(nLRange), numel(xLRange));
-        for ii = 1 : length(IResiduals)
-            iinL = find(nLRange == nLLine(ii));
-            iixL = find(xLRange == xLLine(ii));
-            
-            IResiduals(iinL, iixL) = LineResiduals(ii);
-        end
-        
-    elseif type == "2dline"
-        nLIntercept = nLGrid(xLGrid == 0);
-        xLIntercept = xLGrid(nLGrid == 0);
-        nLDelta = diff(nLGrid(1:2));
-        
-        nLtoxL = nLtoxLLineFun(nLIntercept, xLIntercept, nLDelta);
-        
+    if ~isempty(varargin)
+        % Load by name.
+        filename = varargin{1};
+        load(ResultsPath(sprintf(FILEFORMAT, P.patientCode, filename)), ...
+            'nLGrid', 'xLGrid', 'IResiduals');
     else
-        nLRange = sort(unique(nLGrid));
-        xLRange = sort(unique(xLGrid));
-    end
-    
-    
-elseif isequal(method, 'improve')
-    type = 'grid';
-    
-    % Load by name.
-    loadname = varargin{1};
-    delta = varargin{2};
-    nLPrecision = delta(1);
-    xLPrecision = delta(end);
-    
-    load(ResultsPath(sprintf(FILEFORMAT, P.patientCode, loadname)), ...
-        'nLGrid', 'xLGrid', 'IResiduals');
-    
-    nLRange = sort(unique(nLGrid));
-    xLRange = sort(unique(xLGrid));
-    nLDelta = diff(nLRange(1:2));
-    xLDelta = diff(xLRange(1:2));
-    
-    loop = true;
-    factor = 10;
-    numSegments = 5;
-    while loop
-        % Find optima of current grid.
-        iiOpt = find(IResiduals == min(IResiduals(:)));
-        [iixLOpt, iinLOpt] = ind2sub(size(IResiduals), iiOpt);
-        nLOpt = nLRange(iinLOpt);
-        xLOpt = xLRange(iixLOpt);
-        
-        % Set up new search range around optima.
-        nLBounds = [nLOpt - nLDelta,  nLOpt + nLDelta];
-        xLBounds = [xLOpt - xLDelta,  xLOpt + xLDelta];
-        nLRange = linspace(nLBounds(1), nLBounds(end), numSegments);
-        xLRange = linspace(xLBounds(1), xLBounds(end), numSegments);
-        [xLGrid, nLGrid] = meshgrid(xLRange, nLRange);
-        
-        % Evaluate over grid.
-        savename = sprintf(GRIDFORMAT, ...
-            nLBounds, nLDelta, xLBounds, xLDelta);
-        IResiduals = EvaluateGrid(P, nLGrid, xLGrid, savename);
-        
-        % Update grid settings for next iteration.
-        if (iinLOpt == 1) || (iinLOpt == numSegments) ...
-                || (iixLOpt == 1) || (iixLOpt == numSegments)
-            % Best value is on boundary. Next loop will recenter window
-            % at same precision - do nothing and continue!
-            loop = true;
-            
-        elseif (nLDelta == nLPrecision) && (xLDelta == xLPrecision)
-            % We're at desired precision, and optimum isn't on edge.
-            % Time to finish up.
-            loop = false;
-            
-        else
-            % Increase precision of grid.
-            if nLDelta/factor > nLPrecision
-                nLDelta = nLDelta/factor;
-            else
-                nLDelta = nLPrecision;
-            end
-            
-            if xLDelta/factor > xLPrecision
-                xLDelta = xLDelta/factor;
-            else
-                xLDelta = xLPrecision;
-            end
+        % Load highest resolution grid file we can find.
+        files = dir(ResultsPath(sprintf("%s_grid *", P.patientCode)));        
+        resolutions = [];
+        for ii = 1:length(files)
+            [~, name, ~] = fileparts(files(ii).name);
+            stringbits = split(name, "@");
+            resolutions(ii) = str2double(stringbits(end));
         end
+        
+        [~, iiBest] = min(resolutions);
+        file = files(iiBest);
+        load(fullfile(file.folder, file.name), ...
+            'nLGrid', 'xLGrid', 'IResiduals');
     end
-    
-    
-elseif isequal(method, 'variance')
-    % Load by name.
-    loadname = varargin{1};
-    variance = varargin{2};
-    
-    load(ResultsPath(sprintf(FILEFORMAT, P.patientCode, loadname)), ...
-        'nLGrid', 'xLGrid', 'IResiduals', 'ISimulated');
-    
-    words = split(loadname, ' ');
-    type = words{1};
-    
-    % Prepare array of patients for each nL/xL point.
-    bestPArray = [];
-    worstPArray = [];
-    
-    [~, vITotal] = GetSimTime(P, P.data.ITotal);  % Data [pmol/L]
-    
-    for ii = length(ISimulated)
-        isModelOver = (ISimulated{ii} > vITotal);
-        
-        bestDataVariance = (1+variance)*isModelOver + (1-variance)*~isModelOver;
-        worstDataVariance = (1-variance)*isModelOver + (1+variance)*~isModelOver;
-        
-        bestP = P;
-        bestP.data.ITotal.value = bestP.data.ITotal.value .* bestDataVariance;
-        bestPArray = [bestPArray bestP];
-        
-        worstP = P;
-        worstP.data.ITotal.value = worstP.data.ITotal.value .* worstDataVariance;
-        worstPArray = [worstPArray worstP];
-    end
-    
-    % Evaluate original nL/xL range for both worst and best cases.
-    savename = sprintf("%s best", loadname);  % Best Case
-    EvaluateGrid(bestPArray, nLGrid, xLGrid, savename);
-    
-    savename = sprintf("%s worst", loadname);  % Worst Case
-    EvaluateGrid(worstPArray, nLGrid, xLGrid, savename);
-    
 end
 
 %% Find Optimal nL/xL
@@ -286,158 +79,58 @@ DP = DEBUGPLOTS.FindOptimalHepaticClearance;
 if DP.ErrorSurface
     MakeDebugPlot(P, DP);
     hold on
-    if ~exist('loadname', 'var')
-        loadname = savename;
+    
+    % > Surface
+    % Define surface parameters.
+    nLRange = sort(unique(nLGrid));
+    xLRange = sort(unique(xLGrid));
+    
+    gridMin = min(IResiduals(:));
+    gridMax = max(IResiduals(:));
+    
+    if isfield(P.data, 'stddevMSE')
+        stddevMSE = P.data.stddevMSE;
+    else
+        stddevMSE = 20/100 * gridMin;  % Default to 20%.
     end
     
-    if type == "2dline"
-        % Load original.
-        originalResiduals = IResiduals;
-        plt = plot(nLGrid, originalResiduals);
-        plt.DisplayName = 'Residuals';
-        
-        % Load best/worst files if they exist.
-        bestFile = sprintf(FILEFORMAT, P.patientCode, loadname + " best");
-        if exist(bestFile, 'file')
-            load(ResultsPath(bestFile), ...
-                'nLGrid', 'IResiduals');
-            bestResiduals = IResiduals;
-            plt = plot(nLGrid, bestResiduals);
-            plt.DisplayName = 'Best Case Data Residuals';
-        end
-        
-        worstFile = sprintf(FILEFORMAT, P.patientCode, loadname + " worst");
-        if exist(worstFile, 'file')
-            load(ResultsPath(worstFile), ...
-                'nLGrid', 'IResiduals');
-            worstResiduals = IResiduals;
-            plt = plot(nLGrid, worstResiduals);
-            plt.DisplayName = 'Worst Case Data Residuals';
-        end
-        
-        plt = plot(nLGrid(iiOptimal), minIResidual, 'r*');
-        plt.DisplayName = 'Optimal Point';
-        
-        legend
-        
-        ax1 = gca;
-        ax1.XColor = 'b';
-        ax1XTick = ax1.XTick';
-        
-        ax2 = axes();
-        ax2.XAxisLocation = 'top';
-        ax2.XDir = 'reverse';
-        ax2.Color = 'none';
-        ax2.XColor = 'r';
-        ax2.XTick = linspace(0, 1, length(ax1XTick));
-        ax2.XTickLabel = num2str(flip(nLtoxL(ax1XTick)));
-        ax2.YAxis.Visible = 'off';
-        
-        figTitle = sprintf("%s: Error Line of (I+IDF) Fitting Along $x_L = %.2f - %.2f n_L$", ...
-            P.patientCode, xLIntercept, xLIntercept/nLIntercept);
-        title(figTitle)
-        xlabel(ax1, "$n_L$ [-]");
-        xlabel(ax2, "$x_L$ [1/min]");
-        ylabel(ax1, "2-norm of residuals, $\psi$ [mU/min]")
-        
-        grid on
-        ax1.Position = ax2.Position;
-        
-    else
-        surfaces = {};
-        
-        % Queue original surface.
-        S = struct('IResiduals', IResiduals, ...
-            'nLGrid', nLGrid, ...
-            'xLGrid', xLGrid, ...
-            'name', 'Residuals');
-        surfaces = [surfaces S];
-        
-        % Queue best/worst if they exist.
-        bestFile = ResultsPath(sprintf(FILEFORMAT, P.patientCode, loadname + " best"));
-        if exist(bestFile, 'file')
-            load(ResultsPath(bestFile), ...
-                'nLGrid', 'IResiduals');
-            S = struct('IResiduals', IResiduals, ...
-                'nLGrid', nLGrid, ...
-                'xLGrid', xLGrid, ...
-                'name', 'Best Case Data Residuals');
-            surfaces = [surfaces S];
-        end
-        
-        worstFile = ResultsPath(sprintf(FILEFORMAT, P.patientCode, loadname + " worst"));
-        if exist(worstFile, 'file')
-            load(ResultsPath(worstFile), ...
-                'nLGrid', 'IResiduals');
-            S = struct('IResiduals', IResiduals, ...
-                'nLGrid', nLGrid, ...
-                'xLGrid', xLGrid, ...
-                'name', 'Worst Case Data Residuals');
-            surfaces = [surfaces S];
-        end
-        
-        % Plot each surface.
-        for ii = 1:length(surfaces)
-            nLRange = sort(unique(S.nLGrid));
-            xLRange = sort(unique(S.xLGrid));
-            
-            S = surfaces{ii};
-            subplot(1, length(surfaces), ii)
-            hold on
-                        
-            % > Surface
-            IResiduals = S.IResiduals;
-            gridMin = min(IResiduals(:));
-            gridMax = max(IResiduals(:));
-            
-            if isfield(P.data, 'stddevMSE')
-                stddevMSE = P.data.stddevMSE;
-            else
-                stddevMSE = 20/100 * gridMin;  % Default to 20%.
-            end
-            isWithin1SD = (abs(IResiduals - gridMin) <= stddevMSE);
-            isWithin3SD = ~isWithin1SD & (abs(IResiduals - gridMin) <= 3*stddevMSE);
-            
-            CO(:,:,1) = isWithin3SD * 0.5; %  red
-            CO(:,:,2) = (isWithin1SD|isWithin3SD) * 0.5; % green
-            CO(:,:,3) = ~(isWithin1SD|isWithin3SD) .* (gridMax-IResiduals)/gridMax; % blue
-            caxis([gridMin, gridMin + stddevMSE]);
-            
-            surf(xLRange, nLRange, IResiduals, CO,...
-                'HandleVisibility', 'off', ...
-                'FaceColor', 'interp');
-            
-            % > Contour
-            numLevels = 15;
-            levels = logspace(log10(min(S.IResiduals(:))), log10(max(S.IResiduals(:))), numLevels); % non-linear spacing
-%             levels = linspace(min(S.IResiduals(:)), max(S.IResiduals(:)), numLevels); % linear spacing
-            contour3(xLRange, nLRange, S.IResiduals, ...
-                levels, ...
-                'Color', 'r', ...
-                'HandleVisibility', 'off');
-                        
-%             dim = [0.2 0.5 0.3 0.3];
-%             txt = sprintf("SD = %.2f, %.1f%% of minimum MSE", ...
-%                 P.data.stddevMSE, P.data.stddevMSE/gridMin*100);            
-%             annotation('textbox', dim, 'String', txt, ...
-%                 'FitBoxToText', 'on', ...
-%                 'BackgroundColor', 'white');
-            
-            title(sprintf("%s: %s", P.patientCode, S.name))
-            
-            xlabel("$x_L$ [1/min]")
-            ylabel("$n_L$ [-]")
-            zlabel("Mean of squared errors [(mU/min)^2]")
-        end
-        
-    end
+    % Define colors for different error regions.
+    deltaMSE = abs(IResiduals - gridMin);
+    isWithin1SD = (deltaMSE <= stddevMSE);
+    isWithin3SD = (deltaMSE <= 3*stddevMSE);
+    
+    CO(:,:,1) = (isWithin3SD & ~isWithin1SD) * 0.5; %  red
+    CO(:,:,2) = (isWithin3SD) * 0.5; % green
+    CO(:,:,3) = ~(isWithin3SD) .* (gridMax-IResiduals)/gridMax; % blue
+    
+    % Plot surface.
+    surf(xLRange, nLRange, IResiduals, CO,...
+        'HandleVisibility', 'off', ...
+        'FaceColor', 'interp');
+    
+    % > Contour
+    % Define contour.
+    numLevels = 15;
+    levels = logspace(log10(min(IResiduals(:))), log10(max(IResiduals(:))), numLevels); % non-linear spacing
+    %             levels = linspace(min(IResiduals(:)), max(IResiduals(:)), numLevels); % linear spacing
+    
+    % Plot contour.
+    contour3(xLRange, nLRange, IResiduals, ...
+        levels, ...
+        'Color', 'r', ...
+        'HandleVisibility', 'off');
+    
+    title(sprintf("%s: Error Surface", P.patientCode))
+    
+    xlabel("$x_L$ [1/min]")
+    ylabel("$n_L$ [-]")
+    zlabel("Mean of squared errors [(mU/min)^2]")
 end
 
 end
 
 %% Functions
 function [IResiduals, simI] = EvaluateGrid(PArray, nLGrid, xLGrid, savename)
-global C
 
 global FILEFORMAT
 
@@ -462,16 +155,18 @@ for ii = 1:numel(nLGrid)
     copyP = FitInsulinSensitivity(copyP, false);
     copyP = SolveSystem(copyP);
     
-    % Determine error.
+    % Determine error at raw data points only.
     if (copyP.source == "Detemir")
-        [tI, vI] = GetSimTime(copyP, copyP.data.ITotal);        % Data [pmol/L]
-        simI = C.mU2pmol(copyP.results.I + copyP.results.IDF);  % Sim [mU/L] -> [pmol/L]
+        [tI, vI] = GetSimTime(copyP, copyP.data.ITotal);  % Data [mU/L]
+        simI = copyP.results.I + copyP.results.IDF;       % Sim [mU/L]
     else
-        [tI, vI] = GetSimTime(copyP, copyP.data.I);             % Data [pmol/L]
-        simI = C.mU2pmol(copyP.results.I);                      % Sim [mU/L] -> [pmol/L]
+        [tI, vI] = GetSimTime(copyP, copyP.data.I);  % Data [mU/L]
+        simI = copyP.results.I;                      % Sim [mU/L]
     end
-    iiI = GetTimeIndex(tI, copyP.results.tArray);
-    simI = simI(iiI);
+    
+    simI = simI(ismember(copyP.results.tArray, copyP.data.rawTimes));
+    vI = vI(ismember(tI, copyP.data.rawTimes));
+    
     IErrors = simI - vI;
     
     % Save residuals.
--- 4. Detemir Resimulation/main.m
+++ 4. Detemir Resimulation/main.m
@@ -26,7 +26,7 @@ load config
 % source = "Detemir";
 
 % patientNums = [1 8 5 7 2 3 13 9 10 24];
-patientNums = [1];
+patientNums = [1:50];
 source = "DISST";
 
 patients = makedata(source, patientNums);
@@ -41,14 +41,14 @@ for ii = 1:length(patients)
     
     %% Determine nL/xL.
 %     patients{ii} = FindOptimalHepaticClearance(patients{ii}, ... 
-%         'load','grid nL[-0.1 0.775]@0.025 xL[0.075 0.95]@0.025');  % (nL, xL) by search
+%         'load');  % (nL, xL) by search
     
     patients{ii} = FitHepaticClearance(patients{ii});  % (nL, xL) by MLR
 
     %% Analyse data variance.
-%     stddev = 5/100; 
-%     N = 1000;    
-%     AnalyseInsulinVariance(patients{ii}, stddev, N);    
+    stddev = 5/100; 
+    N = 1000;    
+    AnalyseInsulinVariance(patients{ii}, stddev, N);    
     
     %% Find other dependent parameters. 
     patients{ii} = FindGutEmptyingRate(patients{ii});  % (d2)

536eb350dfdd84e4c45587819c7c2fea4d4211a0
Alex McHugh <alexmchughnz@gmail.com>
2021-01-26 16:52:19 +1300

    Moved some folders around.

--- Code/config.m
+++ Code/config.m
@@ -5,17 +5,17 @@ set(groot, 'DefaultLineLineWidth', 2.0);
 set(groot, 'defaultFigureUnits', 'centimeters', 'defaultFigurePosition', [0 0 8 6]);
 
 global CONFIG
-CONFIG.DATAPATH  = fullfile(pwd, '..', 'Data');
 CONFIG.FUNCPATH  = fullfile(pwd, 'Functions');
 CONFIG.MODELPATH = fullfile(pwd, 'Models');
-CONFIG.RESULTPATH = fullfile(pwd, 'Results');
-CONFIG.SCRIPTPATH = fullfile(pwd, 'Scripts');
+CONFIG.DATAPATH  = fullfile(pwd, '..', 'Data');
+CONFIG.RESULTPATH = fullfile(pwd, '..', 'Results');
+CONFIG.PLOTPATH = fullfile(pwd, '..', 'Plots');
 
-addpath(genpath(CONFIG.DATAPATH));
 addpath(genpath(CONFIG.FUNCPATH));
 addpath(genpath(CONFIG.MODELPATH));
+addpath(genpath(CONFIG.DATAPATH));
 addpath(genpath(CONFIG.RESULTPATH));
-addpath(genpath(CONFIG.SCRIPTPATH));
+addpath(genpath(CONFIG.PLOTPATH));
 
 CONFIG.PATIENTFORMAT = @(P) sprintf("P%s%d", P.source, P.patientNum);
 

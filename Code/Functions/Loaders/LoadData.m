c25d0aa33016c7883b975c7ec3320e3dab2d2cd0
Alex McHugh <alexmchughnz@gmail.com>
2021-01-27 13:22:30 +1300

    Fixed LoadData and moved some debug plots.

--- Code/Functions/Loaders/LoadData.m
+++ Code/Functions/Loaders/LoadData.m
@@ -1,7 +1,6 @@
 function patientSet = LoadData(source, patientNums)
 
-global CONFIG
-
+global CONFIG DEBUGPLOTS
 
 %% Set up patient set.
 for ii = 1:length(patientNums)    
@@ -22,13 +21,43 @@ elseif source == "OGTTLui"
     patientSet = MakeOGTTLui(patientSet);
 end
 
-
-
 %% Save patient structs.
 for ii = 1:length(patientSet)
     P = patientSet{ii};
     save(fullfile(CONFIG.DATAPATH, P.source, P.patientCode), '-struct', 'P');
     fprintf('%s: Saved patient data.\n', P.patientCode);
+end
+
+%% Debug Plots
+DP = DEBUGPLOTS.LoadData;
+if DP.GlucoseInput
+    for ii = 1:length(patientSet)
+        P = patientSet{ii};
+        MakeDebugPlot(P, DP);
+
+        GI = zeros(size(P.results.tArray));
+        GB = zeros(size(P.results.tArray));
+        for tt = 1:length(P.results.tArray)
+            time = P.results.tArray(tt);
+            GI(tt) = GetGlucoseDelivery(time, P);
+            GB(tt) = P.data.GBolus(time);
+        end
+
+        subplot(3,1,1)
+        plot(P.results.tArray, GI)
+        title(sprintf("P%s: G Input", P.patientCode))
+        ylabel("[mmol/min]")
+
+        subplot(3,1,2)
+        plot(P.results.tArray, GB)
+        title(sprintf("P%s: G Bolus", P.patientCode))
+        ylabel("[mmol/min]")
+
+        subplot(3,1,3)
+        plot(P.results.tArray, P.data.GInfusion)
+        title(sprintf("P%s: G Infusion", P.patientCode))
+        ylabel("[mmol/min]")
+    end
 end
 
 end

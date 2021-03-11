85d1fca1ff24b5f11a53e6b77419577e291aa902
Alex McHugh <alexmchughnz@gmail.com>
2021-03-12 11:44:32 +1300

    Fixed ADP for all patterns

--- Code/Functions/Computation/AdjustDataProfile.m
+++ Code/Functions/Computation/AdjustDataProfile.m
@@ -1,4 +1,4 @@
-function P = AdjustDataProfile(P, pattern, varargin)
+function P = AdjustDataProfile(P, patternTime, patternScale, varargin)
 % desc
 % INPUT:
 %   P        - patient struct
@@ -9,23 +9,45 @@ function P = AdjustDataProfile(P, pattern, varargin)
 
 DP = DebugPlots().AdjustDataProfile;
 
-AUC = @(profile) trapz(P.results.tArray, profile);
+AUC = @(time, data) trapz(time, data);
 
 %% Setup
 path = varargin;
- dataProfile = getfield(P, path{:});
+dataProfile = getfield(P, path{:});
 
-expandedPattern = repmat(pattern, 1, ceil(length(dataProfile)/length(pattern)));
-expandedPattern = expandedPattern(1:length(dataProfile));
-expandedPattern = expandedPattern(:);
+ppData = griddedInterpolant(P.results.tArray, dataProfile);
 
-newProfile = dataProfile .* expandedPattern;
+deltaIndex = diff(patternTime(1:2));
+CPepTimes = P.data.CPep.time;
+
+if deltaIndex < 1
+iiUenPoints = GetTimeIndex(CPepTimes, P.results.tArray);
+
+midpoints = iiUenPoints(1:end-1) + round(diff(iiUenPoints).*deltaIndex);
+newIndices = sort([iiUenPoints; midpoints]);
+
+newTime = P.results.tArray(newIndices);
+else
+    newTime = CPepTimes;
+end
+    
+newData = ppData(newTime);
+
+expandedPattern = repmat(patternScale(:), ...
+                          ceil(length(newData)/length(patternScale)), 1);
+expandedPattern = expandedPattern(1:length(newData));
+
+adjustedData = newData .* expandedPattern;
+
+ppNewData = griddedInterpolant(newTime, adjustedData);
+newProfile = ppNewData(P.results.tArray);
+
 P = setfield(P, path{:}, newProfile);
 
 
 %% Optimise
-originalAUC = AUC(dataProfile);
-newAUC = AUC(newProfile);
+originalAUC = AUC(P.results.tArray, dataProfile);
+newAUC = AUC(P.results.tArray, newProfile);
 
 message1 = sprintf("Original AUC: %.2f", originalAUC);
 message2 = sprintf("New AUC: %.2f", newAUC);

dc20689eb01ded6d7c86206eede12cb37e1639ba
Alex McHugh <alexmchughnz@gmail.com>
2021-05-24 13:11:04 +1200

    Persistents now merge.

--- Code/Functions/Helpers/Patient Methods/GetPersistent.m
+++ Code/Functions/Helpers/Patient Methods/GetPersistent.m
@@ -4,7 +4,7 @@ function [P, hasPersistent] = GetPersistent(P, name)
     
     % P may be a modified version of some original P.
     % Reload persistents based on patient code and try again.
-    P = AddPersistents(P);  
+%     P = AddPersistents(P);  
     
     % Check if this struct has persistent saved.
     if isfield(P, "persistents")
--- Code/Functions/Loaders/Builders/AddPersistents.m
+++ Code/Functions/Loaders/Builders/AddPersistents.m
@@ -10,7 +10,10 @@ if isfile(filename)
     loadP = load(fullfile(CONFIG.RESULTPATH, P.source, code));
     
     if isfield(loadP, "persistents")
-        P.persistents = loadP.persistents;
+        fields = fieldnames(loadP.persistents);
+        for ii = 1:numel(fields)
+            P.persistents.(fields{ii}) = loadP.persistents.(fields{ii});
+        end
     end
     
 end

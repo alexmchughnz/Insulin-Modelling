76a03e01c0da6c7f881b86c55fa5b30ec5d19718
Alex McHugh <alexmchughnz@gmail.com>
2021-06-14 12:19:55 +1200

    Generating and plotting higher order splines.

--- Code/Functions/Modelling/Integral Optimisation/Splines/MakeSplineBasisFunctions.m
+++ Code/Functions/Modelling/Integral Optimisation/Splines/MakeSplineBasisFunctions.m
@@ -12,15 +12,49 @@ tSpan = (knots(1) : tDelta : knots(end))';
 phi = zeros(length(tSpan), nSplines, maxOrder+1);  % 3D basis func array.
 
 % Set up zeroth-order splines.
-order = 1;
+order = 0;
 for ii = 1 : length(knots)-1
     isKnotActive = (knots(ii) <= tSpan) & (tSpan < knots(ii+1));
     iiActive = find(isKnotActive);
     
     if ~isempty(iiActive)
-        phi(iiActive, ii, order) = ones(size(iiActive));
+        phi(iiActive, ii, order+1) = ones(size(iiActive));
     end
 end
 
-splines = phi(:,:,1);
-plot(tSpan, splines)
+plotsplines(tSpan, knots, phi, order);
+
+% Recursively define higher order splines.
+for order = 1:maxOrder
+    for ii = 1 : nSplines + (maxOrder-order)
+        prevSplineTerm = (tSpan - knots(ii)) / (knots(ii+order) - knots(ii)) .* phi(:,ii,order);
+        nextSplineTerm = (knots(ii+order+1) - tSpan) / (knots(ii+order+1) - knots(ii+1)) .* phi(:,ii+1,order);
+        
+        phi(:, ii, order+1) = prevSplineTerm + nextSplineTerm;
+    end
+    
+    plotsplines(tSpan, knots, phi, order);
+    
+end
+end
+
+
+function plotsplines(tSpan, knots, phi, order)
+spline = phi(:, :, order+1);
+
+figure(order+1)
+hold on
+
+ylim([0 1])
+
+plot(tSpan, spline);
+
+knotCoords = [knots; knots];
+line(knotCoords, repmat(ylim', 1, length(knotCoords)), ...
+    'Color', 'k', 'LineStyle', ':', 'LineWidth', 0.5)
+
+
+title(sprintf("Order = %d", order))
+xlabel("Time")
+ylabel("Basis Func")
+end

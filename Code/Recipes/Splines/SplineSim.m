230283f0d9ec4be6406839b8b4207453f4a57522
Alex McHugh <alexmchughnz@gmail.com>
2021-11-26 13:56:15 +1300

    Renamed Spline recipes.

---
+++ Code/Recipes/Splines/SplineSim.m
@@ -0,0 +1,64 @@
+function P = SplineSim(P, xL)
+% Recipe for fitting parameters to a model with a fixed xL and splines for
+% nL.
+% INPUTS:
+%   P  - patient struct
+%   xL - (optional) choose xL to fix
+% OUTPUT:
+%   P  - updated patient struct
+
+defaultxL = 0.6;
+
+%% Plots
+plots = DebugPlots();
+
+    plots.EstimateInsulinSecretion.Uen = true;
+    plots.EstimateInsulinSecretion.CPep = true;
+    
+    plots.SolveSystem.CoefficientShapes = false; 
+    
+    plots.MakeSplineBasisFunctions.Splines = false;
+    
+    plots.FitSplines.nLGlucose = true;
+    
+DebugPlots(plots);
+
+
+%% Functions
+P = EstimateInsulinSecretion(P);
+
+% Fix xL to hard-code value.
+if ~exist("xL", "var")
+    xL = defaultxL;
+end
+P.results.xL = xL;
+
+
+% Fit nL with splines over range.
+allowPlots = true;
+P = FitSplinesnL(P, allowPlots);
+
+
+% Find GFast.
+[~, vG] = GetData(P.data.G);
+P.data.GFast = min(vG);
+
+
+% Find d2.
+lbHalfLife = 5;
+ubHalfLife = 95;
+halfLifeRange = 1 ./ linspace(1/ubHalfLife, 1/lbHalfLife, 20);
+d2Range = log(2)./halfLifeRange;
+P = LineSearchOptimum(P, "results.d2", d2Range, @GlucoseError, @FitInsulinSensitivity);
+
+
+% Fit SI.
+P = FitInsulinSensitivity(P);
+
+
+% Solve.
+P = SolveSystem(P, true);
+PlotGlucosenL(P);
+
+end
+

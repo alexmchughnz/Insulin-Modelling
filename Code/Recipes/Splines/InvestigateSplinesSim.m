c36973509e9e00fe653cfc6a810e4e2e657acfad
Alex McHugh <alexmchughnz@gmail.com>
2022-02-28 13:32:45 +1300

    Added Data x2 analysis

--- Code/Recipes/Splines/InvestigateSplinesSim.m
+++ Code/Recipes/Splines/InvestigateSplinesSim.m
@@ -11,34 +11,55 @@ PArray = {};
 
 %% Functions
 % Iterate order with knots at data.
+measTimes = P.data.I.time;
+
 splineOptions.knotType = "location";
-splineOptions.knots = P.data.I.time;
+splineOptions.knots = measTimes;
 
 orderArray = 1:4;
 for ii = 1:numel(orderArray)
     splineOptions.order = orderArray(ii);
-    newP = TagPatientCode(P, "data | order = "+splineOptions.order);
-    newP = SCLossSplineSim(newP, splineOptions);
-    PArray{end+1} = newP;
+    tag = "data | order = "+splineOptions.order;
+    
+    PArray{end+1} = RunSim(P, splineOptions, tag);
+end
+
+% Data, double density.
+splineOptions.knotType = "location";
+
+midpoints = measTimes(1:end-1) + diff(measTimes)/2;
+splineOptions.knots = sort([measTimes(:); midpoints(:)]);
+for ii = 1:numel(orderArray)
+    splineOptions.order = orderArray(ii);
+    tag = "data (x2) | order = "+splineOptions.order;
+    
+    PArray{end+1} = RunSim(P, splineOptions, tag);
 end
 
 
 % Iterate number and order of fixed splines.
 splineOptions.knotType = "amount";
 
-numberArray = [2, 5, 10, 20];
+numberArray = [2, 5, 10, 20, 50];
 orderArray = 1:4;
 for nn = 1:numel(numberArray)
     splineOptions.knots = numberArray(nn);
 
     for kk = 1:numel(orderArray)
         splineOptions.order = orderArray(kk);
-        newP = TagPatientCode(P, "number = " + splineOptions.knots + " | order = " + splineOptions.order);
-        newP = SCLossSplineSim(newP, splineOptions);
-        PArray = [PArray newP];
+        tag = "number = " + splineOptions.knots + " | order = " + splineOptions.order;
+        
+        PArray{end+1} = RunSim(P, splineOptions, tag);
     end
 end
 
+end
+
+
+
+function newP = RunSim(P, splineOptions, tag)
+    newP = TagPatientCode(P, tag);
+    newP = SCLossSplineSim(newP, splineOptions);
 end
 
 
